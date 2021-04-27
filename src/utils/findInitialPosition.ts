aebaff03f5bbe69c8a93a35dd376a9cde1f4f27b
Roza Jiang <junzhujiang@gmail.com>
2021-04-27 23:34:07 +1000

    Make sure upper case is set for directions

--- src/utils/findInitialPosition.ts
+++ src/utils/findInitialPosition.ts
@@ -1,10 +1,5 @@
 import validFirstStep from './validFirstStep';
-
-interface PositionInfo {
-  xPos: number,
-  yPos: number,
-  facingDirection: string
-}
+import PositionInfo from '../types/index';
 
 const findInitialPosition = (inputStep:string):PositionInfo|undefined => {
   const isValidFirstStep = validFirstStep(inputStep);
@@ -15,7 +10,7 @@ const findInitialPosition = (inputStep:string):PositionInfo|undefined => {
     return {
       xPos: positionAsNumber[0],
       yPos: positionAsNumber[1],
-      facingDirection: findFacingDirection[0],
+      facingDirection: findFacingDirection[0].toUpperCase(),
     };
   }
   return undefined;
